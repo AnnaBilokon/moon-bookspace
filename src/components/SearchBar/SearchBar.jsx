16c7b1a88bb5aad2619fbac6944fe67cfdc8288b
AnnaBilokon <Anna.bilokon89@gmail.com>
2024-10-09 14:33:30 +0200

    remove useEffect

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext } from 'react'
 import axios from 'axios'
 import './SearchBar.css'
 import Item from '../Item/Item'
