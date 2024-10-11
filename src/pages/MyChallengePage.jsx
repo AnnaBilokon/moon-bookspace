84628f794182cadefc32a004f9d4b7d1dbd44619
AnnaBilokon <Anna.bilokon89@gmail.com>
2024-10-11 09:59:58 +0200

    MyChallenge component has created

---
+++ src/pages/MyChallengePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Tabs from '../components/Tabs/Tabs'
+
+export default function MyChallengePage() {
+  return (
+    <>
+      <Header />
+      <Tabs />
+    </>
+  )
+}
