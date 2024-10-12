4590766a1511f8b95ce50564644874e77f343ca4
AnnaBilokon <Anna.bilokon89@gmail.com>
2024-10-12 07:55:22 +0200

    add files with data

---
+++ src/genresData.js
@@ -0,0 +1,17 @@
+const genres = [
+    'Literary Fiction',
+    'Historical Fiction',
+    'Non-fiction',
+    'Mystery',
+    'Fantasy',
+    'Detective',
+    'Science Fiction',
+    'Romance',
+    'Classic',
+    'Thriller',
+    'Biography',
+    'Self-help',
+    'Poetry',
+  ]
+
+  export default genres;
