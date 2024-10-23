d250fca931bd7a76575bb419cb9ca3d1ff9cb0b3
AnnaBilokon <Anna.bilokon89@gmail.com>
2024-10-23 16:50:35 +0200

    book page content

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,38 +1,58 @@
 import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
 import { BookContext } from '../../store/book-context'
+import './Book.css'
+import Header from '../Header/Header'
+import StarRating from '../StarRating/StarRating'
 
 export default function Book() {
-  const { modalData } = useContext(BookContext)
+  const { id } = useParams()
+  const { myBooks } = useContext(BookContext)
+
+  const book = myBooks.find((book) => book.id === id)
+
+  if (!book) {
+    return <p>Book not found</p>
+  }
+
   return (
     <>
-      <div className="modal-info-section">
-        <div className="modal-book-image">
-          <img src={modalData.imageLinks?.thumbnail} alt={modalData.title} />
-        </div>
-        <div className="modal-book-titles">
-          <p>
-            <span className="title-style">Title:</span> {modalData.title}
-          </p>
-          <p>
-            <span className="title-style">Author name: </span>{' '}
-            {modalData.authors?.join(', ')}
-          </p>
-          <p>
-            <span className="title-style">Pages:</span> {modalData.pageCount}
-          </p>
-          <p>
-            <span className="title-style">Publishing Date:</span>{' '}
-            {modalData.publishedDate}
-          </p>
-          {/* <div
+      <Header />
+      <div className="current-book-page">
+        <div className="info-book-section">
+          <div className="current-book-image">
+            <img src={book.imageLinks?.thumbnail} alt={book.title} />
+          </div>
+          <div className="current-book-titles">
+            <p className="current-book-title-style ">{book.title}</p>
+            <p className="current-book-author-name">
+              {' '}
+              {book.authors?.join(', ')}
+            </p>
+
+            {/* <div
             className="favorite-icon"
             onClick={() => toggleFavoriteBook(modalData.id)}
           > */}
-          {/* {favoriteBooks.includes(modalData.id) ? (
+            {/* {favoriteBooks.includes(modalData.id) ? (
               <FaHeart style={{ color: 'red' }} /> // Filled heart for favorite
             ) : (
               <FaRegHeart /> // Outlined heart for non-favorite
             )} */}
+          </div>
+        </div>
+        <div className="under-book-section">
+          <StarRating
+            totalStars={5}
+            userRating={book.userRating}
+            className="under-book-info-rating"
+          />
+          <p className="under-book-info-style">Pages: {book.pageCount}</p>
+          <p className="under-book-info-style">
+            Published: {book.publishedDate}
+          </p>
+          <p className="under-book-info-style">Genre: {book.selectedGenre}</p>
+          <p className="under-book-info-style">Read: {book.readDate}</p>
         </div>
       </div>
     </>
